d6539cad26ba978c458a3c5f6884905043b19c7f
Pachev Artem <pachevartem@ya.ru>
2018-06-26 09:33:50 +0300

    before add new sprite

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SupremumStudio
+{
+	
+	public class Teleporter : MonoBehaviour
+	{
+
+		public Transform TeleportZone
+			;
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.tag == "Player")
+			{
+				other.transform.position = TeleportZone.transform.position;
+			}
+		}
+
+
+		private void OnDrawGizmos()
+		{
+			if (gameObject.tag == "Teleport")
+			{
+				Gizmos.DrawLine(transform.position, TeleportZone.position);
+				Gizmos.DrawWireSphere(TeleportZone.position,1);
+			}
+			
+		}
+	}
+}
